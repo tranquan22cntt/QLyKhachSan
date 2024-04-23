d3ba432fa362e0363ad007395f9c5a6f006c46cd
Hien <2224802010038@student.tdmu.edu.vn>
2024-04-23 16:35:19 +0700

    hamm loaddichvu

---
+++ DAO/LoadDVDAO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class LoadDVDAO
+    {
+        private KetNoi ketNoi;
+
+        public LoadDVDAO()
+        {
+            ketNoi = new KetNoi();
+        }
+        public DataTable GetDSDinhVu()
+        {
+            string query = $"SELECT * FROM DICHVU";
+            using (SqlConnection conn = ketNoi.Connect())
+            {
+                using (SqlDataAdapter adapter = new SqlDataAdapter(query, conn))
+                {
+                    DataSet dataSet = new DataSet();
+                    adapter.Fill(dataSet);
+                    return dataSet.Tables[0];
+                }
+            }
+        }
+    }
+}
--- DoAnKhachSanLUXURY/QuanLyHoaDon.cs
+++ DoAnKhachSanLUXURY/QuanLyHoaDon.cs
@@ -1,4 +1,5 @@
-﻿using DevExpress.XtraEditors;
+﻿using BLL;
+using DevExpress.XtraEditors;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,9 +15,11 @@ namespace DoAnKhachSanLUXURY
     public partial class frmQuanLyHoaDon : DevExpress.XtraEditors.XtraForm
     {
         private int quantity = 0;
+        LoadDVBLL themDichVuBLL;
         public frmQuanLyHoaDon()
         {
             InitializeComponent();
+            themDichVuBLL = new LoadDVBLL();
         }
 
         private void btnTang_Click(object sender, EventArgs e)
@@ -37,6 +40,26 @@ namespace DoAnKhachSanLUXURY
         private void UpdateQuantity()
         {
             txtSoLuong.Text = quantity.ToString(); // Hiển thị số lượng trong TextBox
+        }
+
+        private void dgvHoaDonDichVu_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void frmQuanLyHoaDon_Load(object sender, EventArgs e)
+        {
+            LoadDV();
+        }
+
+        private void LoadDV()
+        {
+            dgvHoaDonDichVu.DataSource = themDichVuBLL.GetDichVu();
+        }
+
+        private void btnThemDichVu_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
