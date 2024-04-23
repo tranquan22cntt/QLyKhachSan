0f48deb422feaccceea3974cb90a95c19e3f25b4
phud22cntt03 <tphu27349@gmail.com>
2024-04-23 13:00:06 +0700

    tao class ketnoi

---
+++ DoAnKhachSanLUXURY/KetNoi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAnKhachSanLUXURY
+{
+    internal class KetNoi
+    {
+        private const string ConnectionString = @"Data Source=DESKTOP-JK7J6BR\SQLEXPRESS;Initial Catalog=DoAnKhachSan;Integrated Security=True";
+
+        public SqlConnection Connect()
+        {
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Không kết nối được CSDL: " + e.Message, "Thông báo",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return conn;
+        }
+    }
+}
