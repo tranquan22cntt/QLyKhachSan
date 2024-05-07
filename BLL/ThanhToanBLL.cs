7fe0cec90252705d1f60d39073b0f4b0c7296d06
Hien <2224802010038@student.tdmu.edu.vn>
2024-05-07 09:20:02 +0700

    hàm Thanh Toán

---
+++ BLL/ThanhToanBLL.cs
@@ -0,0 +1,29 @@
+﻿using DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class ThanhToanBLL
+    {
+        private ThanhToanDAO thanhToanDAO;
+
+        public ThanhToanBLL()
+        {
+            thanhToanDAO = new ThanhToanDAO();
+        }
+
+        public bool ThanhToanHoaDonTienPhong(string maHD)
+        {
+            return thanhToanDAO.ThanhToanHoaDonTienPhong(maHD);
+        }
+
+        public bool ThanhToanHoaDonDichVu(string maDV)
+        {
+            return thanhToanDAO.ThanhToanHoaDonDichVu(maDV);
+        }
+    }
+}
